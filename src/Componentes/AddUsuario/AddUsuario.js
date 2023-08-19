da40919b5b914c6d8b4a0665fbe49c4773b5f1c2
j0rdanaaa <j0rdanaaa@gmail.com>
2023-08-19 09:21:45 -0300

    integração com api I template

--- src/Componentes/AddUsuario/AddUsuario.js
+++ src/Componentes/AddUsuario/AddUsuario.js
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function AddUsuario(props) {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
 
+  const addUser = () => {
+
+  const headers = {
+    headers: {
+      Authorization: "jordana-goes-easley",
+    },
+  };
+  const body = {
+    name: nome,
+    email: email,
+  };
+  axios
+    .post(
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+      body,
+      headers
+    )
+    .then(() => {
+      setEmail("")
+      setNome("")
+      props.allUserApi()
+    })
+    .catch((error) => {
+      console.log(error.response.data.message);
+    });
+  }
   return (
     <>
       <h3>Adicionar novo usuario</h3>
@@ -22,7 +49,7 @@ function AddUsuario(props) {
           setEmail(e.target.value);
         }}
       />
-      <button>Enviar</button>
+      <button onClick={addUser}>Enviar</button>
     </>
   );
 }
